cdc0915e54a2f55a8bd4a63946301567f5455fb3
iescheinberg <agr.scheinberg@gmail.com>
2024-05-20 20:30:15 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+/* document.write("Hola");
+console.log("Hola"); */
+
+/* console.log("123");
+console.log(123); */
+
+let var1 = "123";
+let var2 = 123;
+
+console.log(var1);
+console.log(var2);
+
+// var1 = 456;
+console.log(var1);
